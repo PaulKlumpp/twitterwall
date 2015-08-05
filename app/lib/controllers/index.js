9c7435a1c9a1dc2388af25ce011f57eb0f2c1f58
Paul Klumpp <paul@klumpp.net>
2015-08-05 02:47:26 +0200

    also loading random facts controller

--- app/lib/controllers/index.js
+++ app/lib/controllers/index.js
@@ -4,6 +4,7 @@ module.exports = function (app) {
   require('./ScheduleCtrl')(app)
   require('./SiteCtrl')(app)
   require('./SitesCtrl')(app)
+  require('./FactsCtrl')(app)
   require('./TweetCtrl')(app)
   require('./SponsorCtrl')(app)
 }
