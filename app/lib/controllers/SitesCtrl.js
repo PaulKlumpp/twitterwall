468ac459fcee84cf5821796044656e8838714c74
Paul Klumpp <paul@klumpp.net>
2015-08-05 02:47:12 +0200

    cleaning up sites controller

--- app/lib/controllers/SitesCtrl.js
+++ app/lib/controllers/SitesCtrl.js
@@ -5,14 +5,13 @@ var cheerio = require('cheerio')
 module.exports = function (app) {
   app.controller('SitesCtrl', function ($scope, $http, $timeout, $sce) {
     //var sites = $scope.config.sites.sort(Math.random.bind(Math))
-    var sites = $scope.config.sites.sites
+    var siteList = $scope.config.sites.siteList
 
 
     function getAndSaveContent () {
       var snatchIt = ''
       $http.get($scope.sites.url).then(function (res) {
         //console.log('snatched: ' + res)
-
 
         var $ = cheerio.load(res.data)
         var snatchElementName = $scope.sites.snatchElement
@@ -30,18 +29,17 @@ module.exports = function (app) {
         //$scope.sites.content = res.data
 
       })
-
     }
 
     function swap () {
-      $scope.sites = sites[0] // chose first in array
+      $scope.sites = siteList[0] // chose first in array
 
       // now move the first element to the end (with push). And remove the first element (with shift).
-      sites.shift(sites.push(sites[0]))
+      siteList.shift(siteList.push(siteList[0]))
 
       getAndSaveContent()
 
-      $timeout(swap, $scope.sites.duration || 1e4)
+      $timeout(swap, $scope.sites.duration || 15000)
 
     }
 
