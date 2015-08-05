a1fdadeed2d5968e419b72ea166d7f978322d23a
Paul Klumpp <paul@klumpp.net>
2015-08-05 02:46:23 +0200

    random facts controller added

--- app/lib/controllers/FactsCtrl.js
+++ app/lib/controllers/FactsCtrl.js
@@ -1,50 +1,50 @@
 'use strict'
 
-var cheerio = require('cheerio')
+/* global document */
+
+var markdown = require('markdown').markdown
 
 module.exports = function (app) {
-  app.controller('SitesCtrl', function ($scope, $http, $timeout, $sce) {
+  app.controller('FactsCtrl', function ($animate, $scope, $timeout, $sce) {
     //var sites = $scope.config.sites.sort(Math.random.bind(Math))
-    var sites = $scope.config.facts.sites
+    var facts = $scope.config.facts
+    var factList = []
 
+    function randomInterval (low, high) {
+      return Math.floor(Math.random() * (high - low + 1) + low);
+    }
+    function svgify (icon) {
+      return 'https://twemoji.maxcdn.com/svg/' + icon + '.svg'
+    }
 
-    function getAndSaveContent () {
-      var snatchIt = ''
-      $http.get($scope.sites.url).then(function (res) {
-        //console.log('snatched: ' + res)
+    function swapFact () {
+      if (factList.length == 0) {
+        // this array queue is empty, we either have shown all facts or this was a fresh start. populate it!
+        factList = $scope.config.facts.factList.slice() // with slice() it becomes a copy, not a reference
+      }
 
+      var one = randomInterval(0, factList.length - 1)
+      console.log("one: " + one)
 
-        var $ = cheerio.load(res.data)
-        var snatchElementName = $scope.sites.snatchElement
-        var snatchedHtml = ''
-        if (Array.isArray(snatchElementName)) {
-          snatchElementName.forEach(function(entry) {
-            var x = $(entry).toString()
-            snatchedHtml = snatchedHtml + x
-          })
-        } else {
-          snatchedHtml = $(snatchElementName).html()
-        }
+      if (factList[one]) {
+        var fact = factList[one]
+        console.log("fact0: "+ fact )
+        fact = markdown.toHTML(fact)
+        console.log("fact1: "+ fact )
 
-        $scope.sites.content = $sce.trustAsHtml(snatchedHtml);
-        //$scope.sites.content = res.data
+        $scope.fact = $sce.trustAsHtml(fact) // a random from above
 
-      })
+        // now remove that element
+        factList.splice(one, 1) // we're working on the copy 'factList'.
+        console.log("factList.length: "+ factList.length)
+        console.log("$scope.config.facts.factList.length: "+ $scope.config.facts.factList.length)
+
+      }
+
+      $timeout(swapFact, facts.duration || 15000)
 
     }
 
-    function swap () {
-      $scope.sites = sites[0] // chose first in array
-
-      // now move the first element to the end (with push). And remove the first element (with shift).
-      sites.shift(sites.push(sites[0]))
-
-      getAndSaveContent()
-
-      $timeout(swap, $scope.sites.duration || 1e4)
-
-    }
-
-    swap()
+    swapFact()
   })
 }
